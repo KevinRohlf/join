24ea683111d8ec4e2d0911da858e842b017e884a
Chris-Will-Code <christian2006@arcor.de>
2023-03-06 15:03:28 +0300

    summary desktop

---
+++ summary.js
@@ -0,0 +1,30 @@
+function summary() {
+    let content = document.getElementById('content');
+    content.innerHTML = ``;
+    content.innerHTML += htmlTemplateSummary();
+       
+    date();
+    greeting();
+}
+
+
+function date() {
+    newDate = new Date().toLocaleString('en-us',{month:'long', year:'numeric', day:'numeric'});
+    document.getElementById('newDate').innerHTML = newDate;
+}
+
+
+function greeting() {
+    let greet;
+    let myDate = new Date();
+    let hrs = myDate.getHours();
+
+    if (hrs < 12)
+        greet = 'Good Morning';
+    else if (hrs >= 12 && hrs <= 18)
+        greet = 'Good Afternoon';
+    else if (hrs >= 18 && hrs <= 24)
+        greet = 'Good Evening';
+
+    document.getElementById('greeting').innerHTML = greet;
+}
