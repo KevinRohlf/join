d6e09bae2195f9272d4d599b3281db0ca7044f47
Peter <123009795+SeriousPiet@users.noreply.github.com>
2023-03-09 18:44:54 +0100

    added Page 1
    Index.html/.css/.js for Login Page 1

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function fullscreenToCorner () {
+    document.getElementById('capa-1').classList.add('animation');
+    document.getElementById('notAJoinUser').classList.add('einblenden');
+    document.getElementById('login').classList.add('einblenden');
+}
