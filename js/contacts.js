8ff9f2b4b79ee7aa02b98b7f4169ba130e376469
Chris-Will-Code <christian2006@arcor.de>
2023-03-12 16:45:13 +0300

    add contacts

---
+++ js/contacts.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById("contact-btn");
+const overlay = document.getElementById('add-contact-overlay');
+const addContact = document.getElementById('add-contact');
+
+function addContactOpen() {
+overlay.style.display = 'flex';
+addContact.style = 'animation:slide-in .5s ease;';
+btn.disabled = true;
+btn.style.backgroundColor = '#D1D1D1';
+}
+
+
+function addContactClose() {
+addContact.style = 'animation:slide-out .5s ease;';
+setTimeout(() => {
+	overlay.style.display = "none";
+	btn.disabled = false;
+	btn.style.backgroundColor = '#2A3647';
+}, 400);
+}
