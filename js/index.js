d151286b5053004ecf76f1d74a806ca9ee6910e7
Peter <123009795+SeriousPiet@users.noreply.github.com>
2023-03-13 18:42:30 +0100

    changedLogin
    possible right password-Input //
    
    corrected some bugs at buttons and visualization //
    
    added js functions for password input field //
    
    added js functions for initial animation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
-function fullscreenToCorner () {
-    document.getElementById('capa-1').classList.add('animation');
-    document.getElementById('notAJoinUser').classList.add('einblenden');
-    document.getElementById('login').classList.add('einblenden');
+function initial() {
+  document.getElementById("capaOne").classList.add("animation");
+  document.getElementById("capaOneWhite").classList.add("animation");
+  document.getElementById("capaOneContainer").classList.add("ausblenden");
+  if (window.matchMedia("(max-width: 600px)").matches) {
+    document.getElementById("capaOneWhite").classList.add("ausblenden");
+  }
+  setTimeout(() => {
+    let capaOneContainer = document.getElementById("capaOneContainer");
+    capaOneContainer.style.zIndex = "0";
+  }, 1225);
+}
+
+let inputPW = false;
+
+function changePWSymbol() {
+  let pwInputField = document.getElementById("password");
+  let pwSymbol = document.getElementById("pwSymbol");
+  if (pwInputField.value == "") {
+    pwSymbol.src = "assets/img/lock.svg";
+    pwSymbol.classList.remove("cursorPointer");
+    inputPW = false;
+  } else {
+    pwSymbol.src = "assets/img/crossedEye.svg";
+    pwSymbol.classList.add("cursorPointer");
+    inputPW = true;
+  }
+}
+
+function visibilityPW() {
+  let pw = document.getElementById("password");
+  let pwSymbol = document.getElementById("pwSymbol");
+  if (inputPW === true) {
+    if (pw.type === "password") {
+      pw.type = "text";
+      pwSymbol.src = "assets/img/eye.svg";
+    } else {
+      pw.type = "password";
+      pwSymbol.src = "assets/img/crossedEye.svg";
+    }
+  }
 }
