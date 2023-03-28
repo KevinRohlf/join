6f372f430721e018da76ad2b6a034140326354be
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2023-03-28 07:34:55 +0200

    dropdown integrated in board from addtask

--- js/board.js
+++ js/board.js
@@ -54,14 +54,14 @@ let task = [
 let usersFromLocal = []
 let currentTasks = []
 let p = 0
-
+let inAnimation = false;
 
 
 async function loadBackend() {
-    setURL('https://gruppenarbeit-479-join.developerakademie.net/smallest_backend_ever');
+  /*  setURL('https://gruppenarbeit-479-join.developerakademie.net/smallest_backend_ever');
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
-    loadUsersFromStorage()
+    loadUsersFromStorage()*/
     loadTasks()
 }
 
@@ -193,7 +193,7 @@ function renderTaskCategoryColor(i) {
 function loadCard(i) {
     if (window.innerWidth < 1000) {
         main.classList.add('d-none')
-      }
+    }
     body.classList.add('overflow-hidden')
     let overlay = document.getElementById('overlay')
     overlay.classList.remove('d-none')
@@ -259,7 +259,7 @@ function closeCard() {
     document.getElementById('overlay').classList.add('d-none')
     if (window.innerWidth < 1000) {
         main.classList.remove('d-none')
-      }
+    }
 }
 /*
 function renderCardCategoryColor() {
@@ -356,37 +356,86 @@ function searchTasks() {
 
 function loadEditTask(i) {
     renderEditTask(i)
+    renderContacts(i);
+    renderCardContacts(i)
 }
 
 
 function renderEditTask(i) {
     let content = document.getElementById('card-container')
-    content.innerHTML = 
+    content.innerHTML =
     /*html*/ `
     <div>
         <div></div>
-        <form>
+        <form class="edit-task-form">
             <div>
                 <p>Title</p>
                 <input type="text" placeholder="${task[i].title}">
             </div>
             <div>
                 <p>Description</p>
-                <input type="text" placeholder="${task[i].title}">
+                <input type="textarea" placeholder="${task[i].description}">
             </div>
             <div>
                 <p>Due Date</p>
-                <input type="text" placeholder="${task[i].title}">
+                <input type="date" placeholder="${task[i].date}">
             </div>
             <div>
                 <p>Prio</p>
-                <input type="text" placeholder="${task[i].title}">
+                <div class="edit-task-prio-container">
+                    <div onload="highlightPrio()" onclick="changePrio()">
+                    <p>Urgent</p>
+                        <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_39163_1044)">
+                                <path d="M18.9043 14.7547C18.6696 14.7551 18.4411 14.6803 18.2522 14.5412L10.0001 8.458L1.74809 14.5412C1.63224 14.6267 1.50066 14.6887 1.36086 14.7234C1.22106 14.7582 1.07577 14.7651 0.933305 14.7437C0.790837 14.7223 0.653973 14.6732 0.530528 14.599C0.407083 14.5247 0.299474 14.427 0.213845 14.3112C0.128216 14.1954 0.0662437 14.0639 0.0314671 13.9243C-0.00330956 13.7846 -0.0102098 13.6394 0.0111604 13.497C0.0543195 13.2095 0.21001 12.9509 0.443982 12.7781L9.34809 6.20761C9.53679 6.06802 9.76536 5.99268 10.0001 5.99268C10.2349 5.99268 10.4635 6.06802 10.6522 6.20761L19.5563 12.7781C19.7422 12.915 19.8801 13.1071 19.9503 13.327C20.0204 13.5469 20.0193 13.7833 19.9469 14.0025C19.8746 14.2216 19.7349 14.4124 19.5476 14.5475C19.3604 14.6826 19.1352 14.7551 18.9043 14.7547Z" fill="#FF3D00"/>
+                                <path d="M18.9043 9.00568C18.6696 9.00609 18.4411 8.93124 18.2522 8.79214L10.0002 2.70898L1.7481 8.79214C1.51412 8.96495 1.22104 9.0378 0.93331 8.99468C0.645583 8.95155 0.386785 8.79597 0.213849 8.56218C0.0409137 8.32838 -0.0319941 8.03551 0.011165 7.74799C0.054324 7.46048 0.210015 7.20187 0.443986 7.02906L9.3481 0.458588C9.5368 0.318997 9.76537 0.243652 10.0002 0.243652C10.2349 0.243652 10.4635 0.318997 10.6522 0.458588L19.5563 7.02906C19.7422 7.16598 19.8801 7.35809 19.9503 7.57797C20.0204 7.79785 20.0193 8.03426 19.947 8.25344C19.8746 8.47262 19.7349 8.66338 19.5476 8.79847C19.3604 8.93356 19.1352 9.00608 18.9043 9.00568Z" fill="#FF3D00"/>
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_39163_1044">
+                                    <rect width="20" height="14.5098" fill="white" transform="translate(0 0.245117)"/>
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                    <div onload="highlightPrio()" onclick="changePrio()">
+                    <p>Medium</p>
+                        <svg width="20" height="9" viewBox="0 0 20 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_39163_1051)">
+                                <path d="M18.9041 8.22528H1.09589C0.805242 8.22528 0.526498 8.10898 0.320979 7.90197C0.11546 7.69495 0 7.41419 0 7.12143C0 6.82867 0.11546 6.5479 0.320979 6.34089C0.526498 6.13388 0.805242 6.01758 1.09589 6.01758H18.9041C19.1948 6.01758 19.4735 6.13388 19.679 6.34089C19.8845 6.5479 20 6.82867 20 7.12143C20 7.41419 19.8845 7.69495 19.679 7.90197C19.4735 8.10898 19.1948 8.22528 18.9041 8.22528Z" fill="#FFA800"/>
+                                <path d="M18.9041 2.98211H1.09589C0.805242 2.98211 0.526498 2.86581 0.320979 2.6588C0.11546 2.45179 0 2.17102 0 1.87826C0 1.5855 0.11546 1.30474 0.320979 1.09772C0.526498 0.890712 0.805242 0.774414 1.09589 0.774414L18.9041 0.774414C19.1948 0.774414 19.4735 0.890712 19.679 1.09772C19.8845 1.30474 20 1.5855 20 1.87826C20 2.17102 19.8845 2.45179 19.679 2.6588C19.4735 2.86581 19.1948 2.98211 18.9041 2.98211Z" fill="#FFA800"/>
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_39163_1051">
+                                    <rect width="20" height="7.45098" fill="white" transform="translate(0 0.774414)"/>
+                                </clipPath>
+                            </defs>
+                        </svg>
+
+                    </div>
+                    <div onload="highlightPrio()" onclick="changePrio()">
+                    <p>Low</p>
+                        <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 9.00614C9.7654 9.00654 9.53687 8.9317 9.34802 8.79262L0.444913 2.22288C0.329075 2.13733 0.231235 2.02981 0.15698 1.90647C0.0827245 1.78313 0.033508 1.64638 0.0121402 1.50404C-0.031014 1.21655 0.0418855 0.923717 0.214802 0.689945C0.387718 0.456173 0.646486 0.300615 0.934181 0.257493C1.22188 0.21437 1.51493 0.287216 1.74888 0.460004L10 6.54248L18.2511 0.460004C18.367 0.374448 18.4985 0.312529 18.6383 0.277782C18.7781 0.243035 18.9234 0.236141 19.0658 0.257493C19.2083 0.278844 19.3451 0.328025 19.4685 0.402225C19.592 0.476425 19.6996 0.574193 19.7852 0.689945C19.8708 0.805697 19.9328 0.937168 19.9676 1.07685C20.0023 1.21653 20.0092 1.36169 19.9879 1.50404C19.9665 1.64638 19.9173 1.78313 19.843 1.90647C19.7688 2.02981 19.6709 2.13733 19.5551 2.22288L10.652 8.79262C10.4631 8.9317 10.2346 9.00654 10 9.00614Z" fill="#7AE229"/>
+                            <path d="M10 14.7547C9.7654 14.7551 9.53687 14.6802 9.34802 14.5412L0.444913 7.97142C0.210967 7.79863 0.0552944 7.54005 0.0121402 7.25257C-0.031014 6.96509 0.0418855 6.67225 0.214802 6.43848C0.387718 6.20471 0.646486 6.04915 0.934181 6.00603C1.22188 5.96291 1.51493 6.03575 1.74888 6.20854L10 12.291L18.2511 6.20854C18.4851 6.03575 18.7781 5.96291 19.0658 6.00603C19.3535 6.04915 19.6123 6.20471 19.7852 6.43848C19.9581 6.67225 20.031 6.96509 19.9879 7.25257C19.9447 7.54005 19.789 7.79863 19.5551 7.97142L10.652 14.5412C10.4631 14.6802 10.2346 14.7551 10 14.7547Z" fill="#7AE229"/>
+                        </svg>
+                    </div>
+                </div>
             </div>
             <div>
                 <p>Assigned to</p>
-                <input type="text" placeholder="${task[i].title}">
+                <div id="contactShow" class="dropdown">
+                    <div onclick="dropdown('contact')" class="flex gap-10" id="selectContact">
+                        Select contacts to assign
+                    </div>
+                    <div id="contact" class="flex-column d-none">
+                    </div>
+                        <img class="arrow" id="arrowContact" src="./assets/img/arrow-down.svg">
+                </div>
+            </div>
+            <div id="contact-card-container">
             </div>
             <button>
+                <p>OK</p>
                 <svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1 7.5L7 13.5L17 1.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
@@ -396,6 +445,111 @@ function renderEditTask(i) {
     `
 }
 
+function changePrio() {
+    
+}
+
+function highlightPrio() {
+    console.log("test")
+  /*  document.querySelector('#my_area').addEventListener('load', function(){
+        aTestFunction();
+      });
+      
+      function aTestFunction(){
+          return "called";
+      }*/
+}
+
+
+
+
+/*********Edit Task Dropdown Menu************/
+
+
+function dropdown(area) {
+    if (!inAnimation) {
+        let content = document.getElementById(area);
+        let bigArea = area[0].toUpperCase() + area.slice(1);
+        content.classList.remove('d-none')
+        document.getElementById(area + 'Show').style = 'animation: dropdown 2s ease;'
+        document.getElementById(`arrow${bigArea}`).style = 'animation: arrowUp 350ms ease; transform: rotate(180deg);'
+        document.getElementById(`select${bigArea}`).setAttribute('onclick', `dropup('${area}')`);
+        document.getElementById(`arrow${bigArea}`).setAttribute('onclick', `dropup('${area}')`);
+    }
+}
+
+
+function dropup(area) {
+    let content = document.getElementById(area);
+    let areaShow = document.getElementById(area + 'Show')
+    inAnimation = true;
+    let bigArea = area[0].toUpperCase() + area.slice(1);
+    editEndHeight(areaShow);
+    document.getElementById('select' + bigArea).setAttribute('onclick', `dropdown('${area}')`);
+    document.getElementById('arrow' + bigArea).setAttribute('onclick', `dropdown('${area}')`);
+    areaShow.style = 'animation: dropup 500ms ease;';
+    document.getElementById('arrow' + bigArea).style = 'animation: arrowDown 350ms ease;';
+    setTimeout(() => {
+        content.classList.add('d-none');
+        inAnimation = false;
+    }, 500);
+}
+
+
+function renderContacts(i) {
+    let contacts = task[i].contactSelection
+    let content = document.getElementById('contact');
+    content.innerHTML = '';
+    for (let j = 0; j < contacts.length; j++) {
+        let contact = contacts[j];
+        content.innerHTML += `<label for="cb-subtask-${j}"> <div class="contacts">${contact} <input onclick="addContactToList(${j})" id="cb-subtask-${j}" class="subtask-checkbox" type="checkbox" control-id="ControlID-12"></div></label>`
+        if (j == contacts.length - 1) {
+            let j = contacts.length
+            content.innerHTML += `<label for="cb-subtask-${j}"> <div class="contacts">Intive new contact <input onclick="addContactToList(${j})" id="cb-subtask-${j}" class="subtask-checkbox" type="checkbox" control-id="ControlID-12"></div></label>`
+        }
+    }
+}
+
+
+function editEndHeight(content) {
+    document.documentElement.style.setProperty('--end-height', content.clientHeight + 'px')
+}
+
+
+function addContactToList(number) {
+    let contact = document.getElementById(`cb-subtask-${number}`);
+    selectedContacts.push(contacts[number]['contact_selection']);
+    contact.setAttribute('onclick', `removeContactFromList(${number})`);
+    renderSelectedContacts();
+}
+
+
+function removeContactFromList(number) {
+    let contact = document.getElementById(`cb-subtask-${number}`);
+    contact.setAttribute('onclick', `addContactToList(${number})`);
+    let index = selectedContacts.indexOf(contacts[number]['contact_selection']);
+    selectedContacts.splice(index, 1)
+    renderSelectedContacts();
+}
+
+
+function renderSelectedContacts() {
+    let content = document.getElementById('selectContact');
+    content.innerHTML = '';
+    if (selectedContacts.length > 0) {
+        for (let i = 0; i < selectedContacts.length; i++) {
+            let color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+            content.innerHTML += `<div class="circle" style="background-color: ${color.toUpperCase()};": >${selectedContacts[i]}</div>`
+        }
+    } else {
+        content.innerHTML = 'Select contacts to assign';
+    }
+}
+
+
+
+
+/*********Drag and Drop Function************/
 
 
 
